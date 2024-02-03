e652aa71e7e5fec5f0c0744cd3662b36ad6c32b2
Jason C. Leach <jason.leach@fullboar.ca>
2024-02-02 18:57:58 -0800

    fix: use redis cluster (#27)
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- src/redis_config.py
+++ src/redis_config.py
@@ -1,7 +1,9 @@
-import redis
+# import redis
 import os
+from rediscluster import RedisCluster
 
 # Get the Redis URL from environment variables
-redis_url = os.getenv('REDIS_URL')
+redis_uri = os.getenv("REDIS_URI")
 
-redis_instance = redis.from_url(redis_url, decode_responses=True)
+# redis_instance = redis.from_url(redis_url, decode_responses=True)
+redis_instance = RedisCluster.from_url(redis_uri, decode_responses=True)
