2c6292e81e555bd250a09a68e54266097710d04c
Jason C. Leach <jason.leach@fullboar.ca>
2024-04-10 11:31:10 -0700

    chore: script to gen test proof
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/send_proof.py
+++ scripts/send_proof.py
@@ -11,19 +11,19 @@ connection_id = "0c5a9a9f-939a-4332-a0f2-ee0fd1f68442"
 with open(os.path.join("./fixtures/", "request_attest_verif.json"), "r") as f:
     proof_request = json.load(f)
 
-request = create_presentation_request(proof_request)
+create_presentation_response = create_presentation_request(proof_request)
 
-print(json.dumps(request, indent=2))
+# print(json.dumps(create_presentation_response, indent=2))
 
 payload = {
     "connection_id": connection_id,
     "presentation_request": {
-        **request["by_format"]["pres_request"],
+        **create_presentation_response["by_format"]["pres_request"],
     },
 }
 
-print(json.dumps(payload, indent=2))
+# print(json.dumps(payload, indent=2))
 
-pr_response = send_presentation_request(payload)
+send_presentation_response = send_presentation_request(payload)
 
-print(json.dumps(pr_response, indent=2))
+print(f'Send presentation status = {send_presentation_response["state"]}')
