e5ebf39e7dadc2efaac8f3493c5b83d703f54ae3
Jason C. Leach <jason.leach@fullboar.ca>
2024-04-10 11:33:52 -0700

    chore: script to gen test proof
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/send_proof.py
+++ scripts/send_proof.py
@@ -6,11 +6,16 @@ sys.path.insert(0, "./src")
 
 from traction import create_presentation_request, send_presentation_request
 
+# Set this to the id of the connection you want to send the proof to
 connection_id = "0c5a9a9f-939a-4332-a0f2-ee0fd1f68442"
+
+# You may need to adjust the `request_attest_verif.json` data to better match the
+# schema and/or cred_def_id you would like to use in your proof.
 
 with open(os.path.join("./fixtures/", "request_attest_verif.json"), "r") as f:
     proof_request = json.load(f)
 
+# Step 1: Create a presentation request
 create_presentation_response = create_presentation_request(proof_request)
 
 # print(json.dumps(create_presentation_response, indent=2))
@@ -24,6 +29,8 @@ payload = {
 
 # print(json.dumps(payload, indent=2))
 
+# Step 2: Send the presentation request
 send_presentation_response = send_presentation_request(payload)
 
+# If the response is successful, you should see a state of "request_sent"
 print(f'Send presentation status = {send_presentation_response["state"]}')
