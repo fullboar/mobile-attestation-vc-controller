dd3a94c2446fa04897b5989af65f1be0386da9db
Jason C. Leach <jason.leach@fullboar.ca>
2024-05-16 09:38:05 -0700

    feat: proof gen scripts
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/send_proof.py
+++ scripts/send_proof.py
@@ -7,7 +7,9 @@ sys.path.insert(0, "./src")
 from traction import create_presentation_request, send_presentation_request
 
 # Set this to the id of the connection you want to send the proof to
-connection_id = "3ecb6447-fc2a-4291-8079-************"
+connection_id = "9f5d5eff-b01e-476b-8493-66aef5a3e26d"  # P7a
+# connection_id = "c070c375-444d-4caf-93da-d21db735a6e7"  # P8
+# connection_id = "3ecb6447-fc2a-4291-8079-7362dfc564d4"  # i14
 
 # You may need to adjust the `request_attest_verif.json` data to better match the
 # schema and/or cred_def_id you would like to use in your proof.
